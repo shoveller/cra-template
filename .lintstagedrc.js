017751c5959d8824b4049b166b4faec543374aaf
fp91224 <fp91224@linecorp.com>
2022-01-13 07:09:19 +0900

    feat: husky 초기화

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  '**/*.{js,jsx,ts,tsx,json}': [
+    // 타입 체크
+    'tsc --noEmit',
+    // 코딩 컨벤션 체크
+    'npm run lint',
+    // 코드 포맷팅
+    'npm run prettier',
+  ],
+  '**/*.{js,jsx,json}': ['npm run prettier'],
+}
